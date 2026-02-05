efd3740ecc90b1a392a00a2228b6bd7b04239062
David Simba <david.simba1985@gmail.com>
2026-02-05 01:27:34 +0100

    last fixes: 100%

---
+++ src/components/section/phoneChatRoom.js
@@ -0,0 +1,83 @@
+import React, {useState, useEffect } from 'react'
+import Message from './messageCommunizi'
+import PerfectScrollbar from 'react-perfect-scrollbar'
+import { db } from '../../Firebase'
+
+export default function ChatRoom({user, userDB, title}) {
+
+    const [messages, setMessages] = useState([])
+    const [chatRoom, setChatRoom] = useState([])
+
+    useEffect(() => {
+        const chatRoomOnAir = () => {
+            return db.collection('hotels')
+            .doc(userDB.hotelId)
+            .collection("chat")
+            .doc(title)
+            .collection("chatRoom")
+            .orderBy("markup", "desc")
+            .limit(50)
+        }
+
+        let unsubscribe = chatRoomOnAir().onSnapshot(function(snapshot) {
+                    const snapInfo = []
+                  snapshot.forEach(function(doc) {          
+                    snapInfo.push({
+                        id: doc.id,
+                        ...doc.data()
+                      })        
+                    });
+                    setMessages(snapInfo)
+                });
+                return unsubscribe
+     },[title])
+
+     useEffect(() => {
+        const getChatRoom = () => {
+            return db.collection('hotels')
+            .doc(userDB.hotelId)
+            .collection("chat")
+            .where("title", "==", title)
+        }
+
+        let unsubscribe = getChatRoom().onSnapshot(function(snapshot) {
+                    const snapInfo = []
+                  snapshot.forEach(function(doc) {          
+                    snapInfo.push({
+                        id: doc.id,
+                        ...doc.data()
+                      })        
+                    });
+                    setChatRoom(snapInfo)
+                });
+                return unsubscribe
+     },[])
+
+    return (
+        <div>
+            <PerfectScrollbar style={{paddingTop: "3vh"}}>
+                {user&& userDB&& messages.map((flow, key) => {
+                    let language = userDB.language
+
+                    if(userDB.language === chatRoom.guestLanguage) {
+                        return <Message                                 
+                                key={key}
+                                author={flow.author}
+                                photo={flow.photo}
+                                text={flow.text}
+                                markup={flow.markup}
+                                user={user}
+                                title={flow.title}
+                            />
+                    }else{
+                        if(flow.translated){
+                            return renderSwitch()
+                         }
+                    }
+
+                    
+                })}
+            </PerfectScrollbar>
+        </div>
+    )
+}
